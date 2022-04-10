c0634917670cf9a7975cbe949f725f9208483c8b
puppychiang <48985395+puppychiang@users.noreply.github.com>
2022-04-10 15:14:06 +0800

    Add files via upload

---
+++ wwwroot/js/main.js
@@ -0,0 +1,11 @@
+﻿
+$(document).ready(function () {
+    $("input").click(function () {
+        //alert("Hi");
+        //$("H1").text("Hello");
+        let numberListItem = $("li").length;
+        let randomChidNumber = Math.floor(Math.random() * numberListItem);
+        $("H1").text($("li").eq(randomChidNumber).text());
+    });
+});
+
